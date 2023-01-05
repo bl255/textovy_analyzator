321344ef3eac4252d07803ebd7d69b2995a40c96
MV <email@email.email>
2023-01-05 20:01:27 +0100

    grammar

--- languages/sk.py
+++ languages/sk.py
@@ -2,9 +2,9 @@
 
 t_uname_input = "Zadaj meno: "
 t_password_input = "Zadaj heslo: "
-t_uname_error = "Uživateľské meno nie je registrované."
+t_uname_error = "Užívateľské meno nie je registrované."
 t_password_error = "Zadané heslo pre užívateľa {user_name} nie je správne."
-t_wellcome = "Vítaj v aplikacii, {user_name}!\n{text_choices}"
+t_wellcome = "Vítaj v aplikácii, {user_name}!\n{text_choices}"
 t_text_choice = "Na výber je z {num_of_texts} textov."
 t_no_text = "Na výber nie je k dispozícii žiaden text."
 t_text_num = "Zadaj číslo textu (1 až {num_of_texts}): "
